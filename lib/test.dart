cba3acaaa2256d00dee6e2ab0c753e378705defe
Jamshid <jamshidbekormonjonov@gmail.com>
2022-08-05 22:46:36 +0500

    Modul 5 amaliy ish Login ui

--- lib/test.dart
+++ lib/test.dart
@@ -9,155 +9,157 @@ class Testpage extends StatefulWidget {
 }
 
 class _TestpageState extends State<Testpage> {
-  TextEditingController emailController = new TextEditingController();
-  TextEditingController passwordController = new TextEditingController();
-
-  _doSignin() {
-    String email = emailController.text;
-    String password = passwordController.text;
-    if (email.isNotEmpty && password.isNotEmpty) {
-      Navigator.pushNamed(context, Homepage.id);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green,
-        body: Column(
-        children: [
-        Expanded(
-        flex: 3,
-        child: Container(
-        alignment: Alignment.centerLeft,
-        margin: EdgeInsets.only(left: 20, top: 40),
-    child: RichText(
-    text: TextSpan(
-    text: "Login\n",style: TextStyle(color:Colors.white,fontSize: 30,),
-    children: [
-    TextSpan(
-    text: "Welcome Back",style: TextStyle(color: Colors.white,fontSize: 18),
-    ),
-    ]
-    ),
-    ),
-    ),
-    ),
-    Expanded(
-    flex:7,
-    child: Container(
-    //radiusi
-    decoration: BoxDecoration(
-    color: Colors.white,
-    borderRadius: BorderRadius.only(topLeft: Radius.circular(53),topRight: Radius.circular(53)),
-    ),
-    child: Column(
-    children: [
-    SizedBox(height: 100,),
-    Container(
-    margin: EdgeInsets.only(left: 20,right: 20,),
-    decoration: BoxDecoration(
-    borderRadius: BorderRadius.circular(5),
-    color: Colors.white,
-    ),
-    child: TextField(
-    controller: emailController,
-    style: TextStyle(fontSize: 16),
-    decoration: InputDecoration(
-    contentPadding: EdgeInsets.all(10),
-    border: OutlineInputBorder(borderSide:BorderSide(color: Colors.green)),
-    hintText: "Email",
-    hintStyle: TextStyle(color: Colors.black87)
-    ),
-    ),
-    ), Container(
-    margin: EdgeInsets.only(left: 20,right: 20,),
-    decoration: BoxDecoration(
-    borderRadius: BorderRadius.circular(5),
-    color: Colors.white,
-    ),
-    child: TextField(
-    controller: passwordController,
-    style: TextStyle(fontSize: 16),
-    decoration: InputDecoration(
-    contentPadding: EdgeInsets.all(10),
-    border: OutlineInputBorder(borderSide:BorderSide(color: Colors.green)),
-    hintText: "Password",
-    hintStyle: TextStyle(color: Colors.black87)
-    ),
-    ),
-    ),
-    SizedBox(height: 35,),
-    SizedBox(
-    height: 45,
-    width: 220,
-    child: FlatButton(
-    onPressed: (){
-    _doSignin();
-    },
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(Icons.apple,color: Colors.white,),
-        Text("Login",style: TextStyle(color: Colors.white),),
-      ],
-    ),
-    color: Colors.green,
-    shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(25),
-    ),
-    ),
-    ),
-      SizedBox(height: 30,),
-      Container(
-        height: 30,
-        child: GestureDetector(
-          onTap: (){},
-          child: Text("Login with SNS",style: TextStyle(color: Colors.blue),),
+      body: Container(
+        width: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          colors: [
+            Colors.black.withOpacity(0.6),
+            Colors.black.withOpacity(0.2),
+            Colors.black.withOpacity(0.1),
+          ]
+        )
+      ),
+        child: Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 80,),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text("Login",style: TextStyle(color: Colors.white,fontSize: 40),),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child:Text("Welcome Back",style: TextStyle(color: Colors.white,fontSize: 20),) ,
+            ),
+            SizedBox(height: 20,),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft:Radius.circular(50),topRight: Radius.circular(50)),
+                ),
+                child: SingleChildScrollView(
+                  child:Column(
+                  children: [
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25,right: 25,),
+                      child: Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color:Color.fromRGBO(171, 171, 171, 0.7),blurRadius: 20,offset: Offset(0,10),
+                                ),
+                              ]
+                            ),
+                            child: Column(
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    border: Border(bottom: BorderSide(color: Colors.grey.shade400)),
+                                  ),
+                                  child: TextField(
+                                   decoration: InputDecoration(
+                                     hintText: "Email",
+                                     hintStyle: TextStyle(color: Colors.grey,),
+                                     border: InputBorder.none,
+                                   ),
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    border: Border(bottom: BorderSide(color: Colors.grey.shade400)),
+                                  ),
+                                  child: TextField(
+                                   decoration: InputDecoration(
+                                     hintText: "Password",
+                                     hintStyle: TextStyle(color: Colors.grey,),
+                                     border: InputBorder.none,
+                                   ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Container(
+                        margin: EdgeInsets.symmetric(horizontal: 50),
+                        height: 45,
+                       decoration: BoxDecoration(
+                         borderRadius: BorderRadius.circular(50),
+                         color: Colors.green,
+                       ),
+                        child: Center(
+                          child: Text("Login",style: TextStyle(color: Colors.white),),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10,),
+                    GestureDetector(
+                      onTap: (){},
+                      child: Text("Login with SNS",style: TextStyle(color: Colors.grey[600]),),
+                    ),
+                    SizedBox(height: 10,),
+                    Padding(
+                      padding: const EdgeInsets.all(30),
+                      child: Row(
+                        children: [
+                        Expanded(
+                          child: Container(
+                            height:50,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(60),
+                              color: Colors.blue,
+                            ),
+                            child: Center(
+                              child: Text("Facebook",style: TextStyle(color: Colors.white,),),
+                            ),
+                          ),
+                        ),
+                          SizedBox(width: 20,),Expanded(
+                          child: Container(
+                            height:50,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(60),
+                              color: Colors.black,
+                            ),
+                            child: Center(
+                              child: Text("Github",style: TextStyle(color: Colors.white,),),
+                            ),
+                          ),
+                        ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),),
+              ),
+            ),
+
+          ],
         ),
       ),
-      SizedBox(height: 20,),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 45,
-            width: 150,
-            child: FlatButton(
-              onPressed: (){
-                _doSignin();
-              },
-              child: Text("Facebook",style: TextStyle(color: Colors.white),),
-              color: Colors.blue,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-            ),
-          ),
-          SizedBox(width: 30,),
-          SizedBox(
-            height: 45,
-            width: 150,
-            child: FlatButton(
-              onPressed: (){
-                _doSignin();
-              },
-              child: Text("Github",style: TextStyle(color: Colors.white),),
-              color: Colors.black87,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-            ),
-          ),
-        ],
-      ),
-    ],
-    ),
-    ),),
-
-    ],
-    ),
-        );
+    );
   }
 }
 
